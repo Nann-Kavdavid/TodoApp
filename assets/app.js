9843652993feeaba1bd772fb7a09316913d688e1
Nann-Kavdavid <kavdavidnann1993@gmail.com>
2021-12-27 21:53:22 +0900

    ユーザモジュール作成

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,31 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 // import './bootstrap';
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import Home from "./views/Home";
+import App from "./views/App";
+
+const routes = [
+    { path: '/', component: Home, name: 'home'}
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    base: '/app/',
+    routes
+});
+
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+new Vue({
+    router,
+    el: '#app',
+    render: h => h(App)
+})
